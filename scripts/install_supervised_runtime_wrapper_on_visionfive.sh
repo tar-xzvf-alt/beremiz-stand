3129ff09ec7acbbfa54c040f827bd2194ad8df06
Taran Evgeniy <taranev@basealt.ru>
2026-06-16 10:39:35 +0400

    Enable supervised PLC autostart

--- scripts/install_supervised_runtime_wrapper_on_visionfive.sh
+++ scripts/install_supervised_runtime_wrapper_on_visionfive.sh
@@ -19,7 +19,7 @@ export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 export HOME=/root
 cd '$RUNTIME_DIR'
 exec /usr/bin/python3 /usr/share/beremiz/Beremiz_service.py \
-	-i '$BIND_IP' -p '$PORT' -x 0 -t 0 -w off \
+	-i '$BIND_IP' -p '$PORT' -a 1 -x 0 -t 0 -w off \
 	-e '$COMPAT_EXTENSION' .
 EOF
 	chmod +x '$WRAPPER'; \
